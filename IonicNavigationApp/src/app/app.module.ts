9385d1fc7ac18e002ec98a0f252e9bf599b7872f
bgpalmer <brettgpalmer@gmail.com>
2018-10-11 07:27:59 -0600

    Sample application to demo navigation routing in Ionic

---
+++ IonicNavigationApp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, RouteReuseStrategy, Routes } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CompOneComponent } from './components/comp-one/comp-one.component';
+import { CompTwoComponent } from './components/comp-two/comp-two.component';
+import { CompThreeComponent } from './components/comp-three/comp-three.component';
+
+@NgModule({
+  declarations: [AppComponent, CompOneComponent, CompTwoComponent, CompThreeComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
