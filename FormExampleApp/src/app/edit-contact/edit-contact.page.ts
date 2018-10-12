7a0e4aaa1cac6e6944bebaaae37e2890250b6ded
Todd H <tdhale@verisk.com>
2018-10-12 12:09:36 -0600

    add color to navbar, move buttons to navbar, add toggle and date picker

--- FormExampleApp/src/app/edit-contact/edit-contact.page.ts
+++ FormExampleApp/src/app/edit-contact/edit-contact.page.ts
@@ -1,5 +1,5 @@
-import { Contact } from './../../../../FormExampleApp3/src/models/contact.model';
-import { ModalController, NavParams } from '@ionic/angular';
+import { Contact } from './../../models/contact.model';
+import { ModalController, NavParams, Platform } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,11 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class EditContactPage implements OnInit {
 
   contact: Contact;
+  birthdate: any;
 
   constructor(
     private modalController: ModalController,
-    private navParams: NavParams) {
+    private navParams: NavParams,
+    public platform: Platform) {
       this.contact = this.navParams.get('contact');
+      this.birthdate = {
+        year: { value: this.contact.birthdate.getFullYear() },
+        month: { value: this.contact.birthdate.getMonth() + 1 },
+        day: { value: this.contact.birthdate.getDate() }
+      };
     }
 
   ngOnInit() {
@@ -22,6 +29,7 @@ export class EditContactPage implements OnInit {
 
   saveContact() {
     // add validation checks before dismissing
+    this.contact.birthdate = new Date(this.birthdate.year.value, this.birthdate.month.value - 1, this.birthdate.day.value);
     this.modalController.dismiss(this.contact);
   }
 
