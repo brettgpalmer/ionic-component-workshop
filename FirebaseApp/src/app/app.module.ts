33268a0a6d0b3238b1a13e54ab07b397528457ea
Todd H <tdhale@verisk.com>
2018-10-25 16:14:03 -0600

    simple google login working

--- FirebaseApp/src/app/app.module.ts
+++ FirebaseApp/src/app/app.module.ts
@@ -9,14 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { firebaseConfig } from './firebaseConfig';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AngularFireAuth
   ],
   bootstrap: [AppComponent]
 })
--- FirebaseApp/src/app/home/home.page.ts
+++ FirebaseApp/src/app/home/home.page.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  constructor(public afAuth: AngularFireAuth) {
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+}
